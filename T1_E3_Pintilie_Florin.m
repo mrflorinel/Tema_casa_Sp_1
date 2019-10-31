94bb3ad997eb8c53ea1ca48e3fbeccc193bfe109
mrflorinel <57033356+mrflorinel@users.noreply.github.com>
2019-10-31 17:03:05 +0200

    Delete T1_E3_Pintilie_Florin.m

--- T1_E3_Pintilie_Florin.m
+++
@@ -1,8 +0,0 @@
-%Exercitiul 3
-%Se genereaza un vector cu elemente complexe
-%Creem o functie cu 3 parametrii de iesire si unul de intrare, anume
-%vectorul nostru.
-%Aplicand functiile din platforma se ajunge usor la rezultat.
-
-v = [1,1+i,5-3i,7,4-2i]
-functia_mea(v)
